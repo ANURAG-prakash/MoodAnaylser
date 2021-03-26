b46c37506407545cf35ae8c639d088eecd351fe5
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-26 22:35:39 +0530

    Anurag Prakash ; has Added

---
+++ MoodAnaylserWithCore/MoodAnaylser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnaylserWithCore
+{
+    public class MoodAnaylser
+    {
+        public string message;
+        public MoodAnaylser(string message)
+        {
+            this.message = message;
+        }
+        public string anaylseMood()
+        {
+            if (this.message.Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
