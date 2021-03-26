cf4b20031be03b446639ba8f26593cbefa806fa0
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-26 22:58:00 +0530

    anurag Prakash ; has added

---
+++ MoodAnaylserWithCore/MoodAnaylser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnaylserWithCore
+{
+    public class MoodAnaylser
+    {
+        public string message;
+        public MoodAnaylser(string message)
+        {
+            this.message = message;
+        }
+        public string anaylseMood()
+        {
+            try
+            {
+                if (this.message.Contains("sad"))
+                {
+                    return "sad";
+                }
+                else
+                {
+                    return "happy";
+                }
+            }
+            catch
+            {
+                return "happy";
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnaylserWithCore;
+
+namespace MoodAnaylserTestWithCore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            string expected = "sad";
+            string message = "i am in sad mood ";
+            MoodAnaylser moodAnaylse = new MoodAnaylser(message);
+            string mood = moodAnaylse.anaylseMood();
+            Assert.AreEqual(expected, mood);
+        }
+        [TestMethod]
+        public void GivenHappyMoodShouldReturnHappy(string message)
+        {
+            string expected = "happy";
+            MoodAnaylser moodAnaylse = new MoodAnaylser(message);
+            string mood = moodAnaylse.anaylseMood();
+            Assert.AreEqual(expected, mood);
+
+        }
+    }
+}
